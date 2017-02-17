ced661c9c256fb9ad3adf38a9bdcfd9fbc526029
Łukasz <jagoo@post.pl>
2017-02-17 01:35:36 +0100

    basic functionality

--- wp-content/plugins/redirect-anything-pro/assets/js/rap.js
+++ wp-content/plugins/redirect-anything-pro/assets/js/rap.js
@@ -1,12 +1,39 @@
 jQuery( document ).ready( function( $ ) {
 
+	$('#phylax_create_redirect').show();
 	$('#phylax_create_redirect').accordion( {
 		collapsible: true,
 		active: phylax_rap_active,
+		heightStyle: 'content',
 		icons: {
 			header: 'phylax_disabled',
 			activeHeader: 'phylax_enabled'
+		},
+		activate: function( event, ui ) {
+			inuse = ui.newHeader.data('rapinuse');
+			if ( typeof inuse == 'undefined' ) {
+				inuse = 0;
+			}
+			$('#pbrap_in_use').val( inuse );
 		}
+	} );
+
+	$('.pbrapsel').on( 'click', function() {
+		$('.pbrapsel').removeClass('pbrapselact');
+		$(this).addClass('pbrapselact');
+		$('#pbrapsel_select').val( $(this).attr('id') );
+	} );
+
+	$('#rap_list_select select').on( 'change', function() {
+		$('.pbrapsel').removeClass('pbrapselact');
+		$(this).prev().addClass('pbrapselact');
+		$('#pbrapsel_select').val( $(this).prev().attr('id') );
+	} );
+
+	$('#rap_list_select input[type="text"]').on( 'input', function() {
+		$('.pbrapsel').removeClass('pbrapselact');
+		$(this).prev().addClass('pbrapselact');
+		$('#pbrapsel_select').val( $(this).prev().attr('id') );
 	} );
 
 } );
--- wp-content/plugins/redirect-anything-pro/redirect-anything-pro.php
+++ wp-content/plugins/redirect-anything-pro/redirect-anything-pro.php
@@ -20,9 +20,13 @@ class Plugin {
 
 	private $o;
 	private $screen;
+	private $nonce;
+	private $nonce_action;
 
 	function __construct() {
 		if ( is_admin() ) {
+			$this->nonce = 'phylax_rap_nonce';
+			$this->nonce_action = 'phylax_rap_nonce_action';
 			$this->dashboard();
 		}
 	}
@@ -46,6 +50,7 @@ class Plugin {
 	function dashboard() {
 		$this->read_options();
 		add_action( 'add_meta_boxes', [ $this, 'add_meta_boxes' ] );
+		add_action( 'save_post', [ $this, 'save_post' ] );
 		add_action( 'current_screen', [ $this, 'current_screen' ] );
 	}
 
@@ -70,21 +75,125 @@ class Plugin {
 		);
 	}
 
+	function get_posts( $post_type = 'post' ) {
+		return get_posts( [
+			'posts_per_page' => 0,
+			'orderby' => 'name',
+			'order' => 'ASC',
+			'post_type' => $post_type,
+		] );
+	}
+
+	function get_posts_option( $post_type = 'post', $current = 0 ) {
+		$html = '';
+		$items = $this->get_posts( $post_type );
+		if ( is_array( $items ) && ( count( $items ) > 0 ) ) {
+			foreach( $items as $item ) {
+				$title = trim( $item->post_title );
+				if ( '' == $title ) {
+					$title = get_permalink( $item->ID );
+				}
+				$html.= '<option value="' . $item->ID . '">' . $title . '</option>' . PHP_EOL;
+			}
+		}
+		return $html;
+	}
+
+	function post_types_array( $items ) {
+		if ( !is_array( $items ) ) { return []; }
+		if ( count( $items ) < 1 ) { return []; }
+		$r = [];
+		foreach( $items as $item ) {
+			$r[ $item->name ] = $item->label;
+		}
+		return $r;
+	}
+
+	function get_post_types() {
+		$built_in = $this->post_types_array( get_post_types( [ 'public' => true, '_builtin' => true ], 'objects' ) );
+		$others = $this->post_types_array(get_post_types( [ 'public' => true, '_builtin' => false ], 'objects' ) );
+		return array_merge( $built_in, $others );
+	}
+
+	function save_post( $post_id ) {
+		if ( !isset( $_POST[ $this->nonce ] ) ) { return; }
+		if ( !wp_verify_nonce( $_POST[ $this->nonce ], $this->nonce_action ) ) { return; }
+		if ( !post_type_exists( $_POST['post_type'] ) ) { return; }
+		if ( !current_user_can( 'edit_post', $post_id ) ) { return; }
+		$cpid = [];
+		foreach( $_POST as $key => $value ) {
+			if ( ( substr( $key, 0, 4 ) == 'pbs_' ) && ( substr( $key, -3 ) == '_id' ) ) {
+				$cpid[ $key ] = (int)$value;
+			}
+		}
+		$rap = [
+			'use' => esc_attr( $_POST['pbrap_in_use'] ),
+			'basic' => [
+				'selector' => esc_attr( $_POST['pbrapsel_select'] ),
+				'direct' => esc_attr( $_POST['pbs_phylax_direct'] ),
+				'custom_posts' => $cpid,
+			],
+		];
+		#echo '<pre>'.print_r($rap,true).'</pre>';
+		#echo '<pre>'.print_r($_POST,true).'</pre>';
+		#die('OK!');
+		update_post_meta( $post_id, 'phylax_redirect_anything_pro', $rap );
+	}
+
 	function rap_view( $post ) {
+		$rap = get_post_meta( $post->ID, 'phylax_redirect_anything_pro', true );
+		if ( !isset( $rap['use'] ) ) { $rap['use'] = '0'; }
 		$rap_active = 'false'; #
+		if ( $rap['use'] == 'basic' ) { $rap_active = 0; }
+		if ( $rap['use'] == 'advanced' ) { $rap_active = 1; }
+		if ( !isset( $rap['basic'] ) ) { $rap['basic'] = []; }
+		if ( !isset( $rap['basic']['selector'] ) ) { $rap['basic']['selector'] = ''; }
+		if ( !isset( $rap['basic']['direct'] ) ) { $rap['basic']['direct'] = ''; }
+
+		echo '<pre>'.print_r($rap,true).'</pre>';
+
+		wp_nonce_field( $this->nonce_action, $this->nonce );
 ?>
-		<p><?php _e( 'Here you can make this item redirect anywhere and whenever you want. Please select what you want to do, green indicator will let you know where you are.', TD ); ?></p>
+		<p><small><?php _e( 'Here you can make this item redirect anywhere and whenever you want. Please select what you want to do, green indicator will let you know where you are.', TD ); ?></small></p>
 		<script type="text/javascript">
 			var phylax_rap_active = <?php echo $rap_active; ?>;
 		</script>
+		<input class="phylax_rap_hide" type="text" name="pbrap_in_use" id="pbrap_in_use" value="<?php echo $rap['use']; ?>">
 		<div id="phylax_create_redirect">
-			<h3><?php _e( 'Basic (simple)', TD ); ?></h3>
+			<h3 data-rapinuse="basic"><?php _e( 'Basic (simple)', TD ); ?></h3>
 			<div>
-				Basic
+				<p><?php _e( 'Redirect this page to:', TD ); ?></p>
+				<ul id="rap_list_select">
+<?php
+	$post_types = $this->get_post_types();
+	if ( is_array( $post_types ) && ( count( $post_types ) > 0 ) ) {
+		foreach( $post_types as $post_type => $post_name ) {
+			$options = $this->get_posts_option( $post_type );
+			if ( '' != $options ) {
+?>
+					<li><label for="pbrap_<?php echo $post_type; ?>">
+						<div class="pbrapsel" id="pbs_<?php echo $post_type; ?>"><?php echo $post_name; ?></div>
+						<select name="pbs_<?php echo $post_type; ?>_id">
+<?php
+	echo $options;
+?>
+						</select>
+					</label></li>
+<?php
+			}
+		}
+	}
+?>
+					<li><label for="pbrap_phylax_direct">
+						<div class="pbrapsel" id="pbs_phylax_direct"><?php _e( 'Direct link', TD ); ?></div>
+						<input type="text" name="pbs_phylax_direct" value="<?php echo $rap['basic']['direct']; ?>" placeholder="<?php _e( 'https://example.com/', TD ); ?>">
+					</label></li>
+				</ul>
+				<input class="phylax_rap_hide" id="pbrapsel_select" type="text" name="pbrapsel_select" value="<?php echo $rap['basic']['selector']; ?>">
 			</div>
-			<h3><?php _e( 'Advanced (conditional)', TD ); ?></h3>
+			<h3 data-rapinuse="advanced"><?php _e( 'Advanced (conditional)', TD ); ?></h3>
 			<div>
-				Advanced
+				<p><small><?php _e( 'So, I assume you are advanced user of WordPress. Then we give you a few more options but it is a bit trickier than in basic functionality. Of course it is much more powerful and flexible solution.', TD ); ?></small></p>
 			</div>
 		</div>
 <?php
